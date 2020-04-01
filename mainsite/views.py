3e6aa47c63b8357c5e9e3f397cba28b9f64d6a98
SevvyP <45738242+SevvyP@users.noreply.github.com>
2020-04-01 16:59:43 -0400

    Update views.py

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-  return HttpResponse("This is the main page. We can add some buttons or something. Hello")
+  return HttpResponse("This is the main page. We can add some buttons or something.")
