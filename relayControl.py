bfacffc6670d57b1b296c341bc77ab9f8282c2c4
HIDEPillowTalk <HIDEKeithProject@gmail.com>
2020-12-02 18:35:11 -0500

    renamed class for motorControl.py that fits the project better

--- relayControl.py
+++ relayControl.py
@@ -2,30 +2,40 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setwarnings(False)
 
-class MotorControl:
+class RelayControl:
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
 		self.in1 = 11
 		self.in2 = 13
+		self.in3 = 15
+		self.in4 = 16
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(self.in1, GPIO.OUT)
 		GPIO.setup(self.in2, GPIO.OUT)
 
-	def motorOn(self, time, motorNumber):
-		if motorNumber == 1:
+	def relayRun(self, time, relay):
+		if relay == 1:
 			GPIO.output(self.in1, True)
-		elif motorNumber == 2:
+		elif relay == 2:
 			GPIO.output(self.in2, True)
+		elif relay == 3:
+			GPIO.output(self.in3, True)
+		elif relay == 4:
+			GPIO.output(self.in4, True)
+
 		for x in range(time):
 			sleep(1)
-			print "Motor " + str(motorNumber) + " " + str(x)
+			print "Relay " + str(relay) + " " + str(x)
 
-		if motorNumber == 1:
+		if relay == 1:
 			GPIO.output(self.in1, False)
-		elif motorNumber == 2:
+		elif relay == 2:
 			GPIO.output(self.in2, False)
-		GPIO.output(self.in1, False)
-		print "Motor Finished"
+		elif relay == 3:
+			GPIO.output(self.in3, False)
+		elif relay == 4:
+			GPIO.output(self.in4, False)
+		print "Relay Finished"
 
 	def exit(self):
 		GPIO.cleanup()
