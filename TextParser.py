b9b6a728fb259450df8beeab47775de7d784f9b8
Hide <HideKeithProject@gmail.com>
2021-09-21 21:01:03 -0400

    added methods

--- TextParser.py
+++ TextParser.py
@@ -1,2 +1,17 @@
 class TextParser:
     def __init__(self):
+
+    def saveCommand(self, command):
+        counter = 0
+	try:
+            f = open("commandsDB", 'r'):
+	    for line in f:
+		pass
+	    if line != None:
+		if word.isDigit(command.split()[0])
+		    counter = int(word)
+		    break
+        except IOError:
+
+	finally:
+	    f.close()
