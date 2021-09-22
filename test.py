6faadb0a22262d08986f7dfecb8f92b1ce26a589
Hide <HideKeithProject@gmail.com>
2021-09-21 22:01:56 -0400

    changed test program

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-#requires python2.7
-# importing flask module
-from flask import Flask
-
-# initializing a variable of Flask
-app = Flask(__name__)
-
-
-# decorating index function with the app.route
-@app.route('/')
-def index():
-   return "WELCOME!!! This is the home page"
-
+from TextParser import *
 if __name__ == "__main__":
-   app.run()
+    text = TextParser()
+    text.saveCommand("This is a string 2")
