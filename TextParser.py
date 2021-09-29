337d58dfcbe9cf1a82b63a4c950860d3981d78ea
Hide <HideKeithProject@gmail.com>
2021-09-28 20:09:29 -0400

    expanded function

--- TextParser.py
+++ TextParser.py
@@ -6,7 +6,7 @@ class TextParser:
 	try:
 	    f = open("commandDB", 'r')
 	except IOError:
-	    print("file cannot be read")
+	    pass
 	else:
             for line in f:
                 pass
@@ -21,20 +21,36 @@ class TextParser:
 	    print("cannot open commandDB. Major Issue")
 	else:
 	    counter = counter + 1
-	    f.write("{0}: '{1}' {2}\n".format(counter, command, datetime.datetime.now()))
+	    f.write("{0}: '{1}' {2}\n".format(counter, string, datetime.datetime.now()))
 	    f.close()
 
-    def commandSearch(self, string, keyword = None,):
-	#self.saveString(string)
+    # return -2 command not long enough
+    # return -1 keyword not found
+    # return command 
+    def commandSearch(self, string, keyword = None):
 	words = string.split()
 	command = None
-	if keyword != None:
+	if keyword !=None:
 	    try:
 	    	index = words.index(keyword)
-	    	command = words[index:index+4]
+	    	command = words[index+1:index+5]
 	    	print("Command: {0}".format(command))
 	    	print("found keyword '{0}' at index {1} in string '{2}'".format(keyword, index, string))
-	    except ValueError:
-		print("Index does not exist")
+	    except ValueError as e:
+	    	#self.saveString("Keyword:'{0}' not found in '{1}'".format(keyword, string)
+	    	print("Index does not exist")
+	    	return -1
 	else:
-	    print("no keyword\n")
+	    command = words
+
+	if len(command) < 3:
+	    return -2
+	return command
+
+    def returnRelay(self, command):
+	action = command[0]
+	pillow = command[1]
+	time = command[2]
+	if len(command) = 4:
+	    if command[3] = "minute":
+		time = time*60
