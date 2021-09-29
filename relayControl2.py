c264fb7a639af42db7426dacb9370804527ae8a8
Thawng Hmung <tthmung@mtu.edu>
2021-09-28 20:55:01 -0400

    Update relayControl2.py
    Delete unnecessary stuff

--- relayControl2.py
+++ relayControl2.py
@@ -1,4 +1,3 @@
-import RPi.GPIO as GPIO
 from time import sleep
 import itertools
 import smbus
@@ -29,27 +28,13 @@ class RelayControl:
 		if relay == 1:
 			bus.write_byte_data(DEVICE_ADDR, 1, 0xFF)
 			bus.write_byte_data(DEVICE_ADDR, 2, 0x00)
-			# Make sure that right pillow is off
-			bus.write_byte_data(DEVICE_ADDR, 3, 0x00)
-			bus.write_byte_data(DEVICE_ADDR, 4, 0x00)
 		elif relay == 2:
 			bus.write_byte_data(DEVICE_ADDR, 1, 0x00)
 			bus.write_byte_data(DEVICE_ADDR, 2, 0xFF)
-			# Make sure that right pillow is off
-			bus.write_byte_data(DEVICE_ADDR, 3, 0x00)
-			bus.write_byte_data(DEVICE_ADDR, 4, 0x00)
 		elif relay == 3:
-			# Make sure that left pillow is off
-			bus.write_byte_data(DEVICE_ADDR, 1, 0x00)
-			bus.write_byte_data(DEVICE_ADDR, 2, 0x00)
-
 			bus.write_byte_data(DEVICE_ADDR, 3, 0xFF)
 			bus.write_byte_data(DEVICE_ADDR, 4, 0x00)
 		elif relay == 4:
-			# Make sure left pillow is off
-			bus.write_byte_data(DEVICE_ADDR, 1, 0x00)
-			bus.write_byte_data(DEVICE_ADDR, 2, 0x00)
-
 			bus.write_byte_data(DEVICE_ADDR, 3, 0x00)
 			bus.write_byte_data(DEVICE_ADDR, 4, 0xFF)
 
@@ -59,40 +44,7 @@ class RelayControl:
 
 		print "Relay Finished"
 
-	#cycle everything based on given number of loops
-	# def cycleLoop(self, InputTime, outputTime, waitTime, loopNumber):
-	# 	totalTime = InputTime + outputTime + waitTime
-	# 	print "Starting cycle with estimated cycle total time of " + convertSecs(loopNumber * totalTime)
-	# 	for x in range(loopNumber):
-	# 		GPIO.output(self.pin1, True)
-	# 		GPIO.output(self.pin3, True)
-	# 		sleep(cushionTime)
-	# 		GPIO.output(self.pin1, False)
-	# 		GPIO.output(self.pin3, False)
-	# 		sleep(waitTime)
-	# 		GPIO.output(self.pin2, True)
-	# 		GPIO.output(self.pin4, True)
-	# 		sleep(cushionTime-1)
-	# 		GPIO.output(self.pin2, False)
-	# 		GPIO.output(self.pin4, False)
-	# 	print "Cycle Complete"
-		
-	#cycle everything based on given number of hours
-            #def cycleTime(self, InputTime, outputTime, waitTime, totalHours):
-            #	totalTime = InputTime + outputTime + waitTime
-	#cycle each individual pillow based on given number of hours
-	#def inflateMultiPillow(self, timeInput, timeOutput, timeWait, totalHours):
-	#	if (len(timeInput) != len(timeOutput) or len(timeInput) != len(timeWait)):
-	#		return error
-     # 		seconds = convertHours(totalHours)
-      #		totalLoops = []
-      #		for (i, o, w) in zip(timeInput, timeOutput, timeWait)
-       # 		t = i + o + w
-        #  		totalLoops.append(seconds/t)
-#Finish here!!!
-
 	#convert seconds given into hour,minute,sec
-
 	def convertSecs(seconds):
 		seconds = seconds % (24 *3600)
 		hour = second // 3600
@@ -104,7 +56,3 @@ class RelayControl:
 	# convert hours to seconds
 	def convertHours(hours):
 		return hours*60*60
-
-	# Reset the pins mode and clean up any changed settings on the pins
-	# def exit(self):
-	# 	GPIO.cleanup()
