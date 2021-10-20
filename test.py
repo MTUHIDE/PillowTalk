adafd6eb928bc44d54b812cc2bbdacedaec94be0
Ian <hidekeithproject@gmail.com>
2021-10-19 20:10:00 -0500

    test all motors

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 from TextParser import *
+from MotorControl import *
+
 if __name__ == "__main__":
-    text = TextParser()
-    text.saveCommand("This is a string 2")
+	try:
+		motorControl = MotorControl()
+		print(motorControl.relayRun(5, 1))
+		print(motorControl.relayRun(5, 2))
+		print(motorControl.relayRun(5, 3))
+		print(motorControl.relayRun(5, 4))
+	except KeyboardInterrupt:
+		print("stopped")
+		motorControl.stopAll()
