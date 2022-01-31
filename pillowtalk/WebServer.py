56791e679a6b6c50e89056de10417ae9495ddfba
Mark Washington <markdw88@gmail.com>
2022-01-31 16:14:26 -0500

    Update launch.json and start on WebServer.py

--- pillowtalk/WebServer.py
+++ pillowtalk/WebServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, request
 from waitress import serve
 
 from threading import Thread
@@ -22,8 +22,20 @@ def index():
     return render_template("index.html", context=context)
 
 
-@app.route("/settings")
+@app.route("/settings", methods=["POST"])
 def settings():
+    if request.method == 'POST':
+        with open("settings.json", "r+") as f:
+            context = json.load(f)
+            context["cushion_1_nickname"] = request.form["cushion_1_nickname"]
+            context["cushion_2_nickname"] = request.form["cushion_2_nickname"]
+            context["cushion_1_time"] = request.form["cushion_1_time"]
+            context["cushion_2_time"] = request.form["cushion_2_time"]
+            
+            json.dump(context, f)
+    
+        return make_response()
+            
     '''
     Send stuff here to update settings
     '''
