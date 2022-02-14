f58cc66339c565807734b2b2941c90c649519062
iplawrie <iplawrie@mtu.edu>
2022-02-14 17:58:35 -0500

    continued textparser work

--- pillowtalk/TextParser.py
+++ pillowtalk/TextParser.py
@@ -1,5 +1,6 @@
 import datetime
-from MotorControl import *
+from MotorControl import * as MC
+
 
 class TextParser:
     def __init__(self):
@@ -7,9 +8,10 @@ class TextParser:
         if self._dictionary == -1:
             self._dictionary = None
             print("dictionary was not pulled")
-        self._MC = MotorControl()
 
-    # return -1 Error opening file
+    def getDictValues(self, key):
+        return self._dictionary[key]
+
     def openDictionary(self):
         f = None
         keys = {}
@@ -31,85 +33,102 @@ class TextParser:
             f.close()
             return keys
 
-    # return -2 command not long enough
-    # return -1 keyword not found
+# Main function to run
+    def runCommands(self, string, keyword=False):
+        commandArray = self.commandSearch(string, keyword)
+        if commandArray == 0:
+            MC.stopAll()
+            print("stopping motors")
+        
+        returnedMotors = returnMotor(commandArray)
+
     # return 0
     # return command
-    def commandSearch(self, string, keyword=None):
-        words = string.split()
-        command = None
-        if keyword != None:
+    def commandSearch(self, string):
+        words = string.lower().split()
+        listOfCommands = None
+        keywordValues = self.getDictValues("Wakewords")
+        for i, value in enumerate(keywordValues):
             try:
-                index = words.index(keyword)
-                command = words[index + 1 : index + 5]
-                print("Command: {0}".format(command))
-                print("found keyword '{0}' at index {1} in string '{2}'".format( keyword, index, string))
-            except ValueError as e:
-                # self.saveString("Keyword:'{0}' not found in '{1}'".format(keyword, string)
-                raise KeywordNotFoundError(f"Invalid keyword: index {index} does not exist")
-        else:
-            command = words
+                index = words.index(value)
+                listOfCommands = words[index + 1:]
+                break
+            except ValueError:
+                if i == len(keywordValues)-1:
+                    raise KeywordNotFoundError("Keyword Not Found")
+                continue
 
-        if len(command) < 3:
-            raise InsufficientCommandLengthError(f"Command '{command}' Not Long Enough")
-        return command
+        if len(listOfCommands) <= 0 :
+            raise InsufficientCommandLengthError("Command Not Long Enough")
+        return listOfCommands
 
-    # return -1 invalid number
-    # return -2 invalid action
-    # return -3 invalid inflatable
     # bus 1 inflate pillow 1, bus 2 deflate pillow 1, bus 3 inflate pillow 2, bus 4 deflate pillow 2
-    def returnRelay(self, command):
-        relay = None
-        action = command[0]
-        pillow = command[1]
-        time = int(command[2])
-
-        if len(command) == 4:
-            if command[3] == "minute":
-                time = time * 60
-        if action == "inflate":
-            if pillow == "left" or pillow == "cushion_1":
-                relay = 1
-            elif pillow == "right" or pillow =="write" or pillow == "cushion_2":
-                relay = 3
+    def returnMotor(self, commands):
+        commands = commands[::-1]
+        motor1 = None
+        motor2 = None
+        action = None
+        #get correct action
+        values = self.getDictValues("FunctionOff")
+        for value in values:
+            try:
+                commands.index(value)
+                MC.stopAll()
+                return 0
+            except ValueError:
+                continue
+        values = self.getDictValues("Functions")
+        for i, value in enumerate(values):
+            try:
+                commands.index(value)
+                action = value
+                break
+            except ValueError:
+                if i == len(values)-1:
+                    raise InvalidActionError("Keyword Not Found")
+                continue
+            if action == "inflate":
+                motor1 = 1
+                motor2 = 3
             else:
-                raise NonexistentPillowError(f"Pillow {pillow} does not exist")
-        elif action == "deflate":
-            if pillow == "left" or pillow == "cushion_1":
-                relay = 2
-            elif pillow == "right" or pillow =="write" or pillow == "cushion_2":
-                relay = 4
-            else:
-                raise NonexistentPillowError(f"Pillow {pillow} does not exist")
-        else:
-            raise InvalidActionError(f"Action '{action}' is invalid")
+                motor1 = 2
+                motor2 = 4
 
-        return [relay, time]
-
-    
-    # return -2 command not long enough - not enough arguments provided for a command
-    # return -1 keyword not found
-    # return -2 invalid action - the function the pillow needs to take to inflate or deflate
-    # return -3 invalid inflatable - does a pillow not exist?
+        values = self.getDictValues("Pillow")
+        for i, value in enumerate(values):
+            try:
+                index = commands.index(value)
+            except:
+                if i == len(values)-1:
+                    raise InvalidActionError("Keyword Not Found")
+                continue
+        #time
+        #units
+        return [motor1, motor2, time]
 
 class InsufficientCommandLengthError(Exception):
     '''Catch the lack of required arguments for a command'''
+
     def __init__(self, message):
         super().__init__(message)
 
-    
+
 class KeywordNotFoundError(Exception):
     '''Catch a keyword that does not exist'''
+
     def __init__(self, message):
         super().__init__(message)
 
-    
+
 class InvalidActionError(Exception):
     '''Catch an invalid function the pillow needs to inflate or deflate'''
+
     def __init__(self, message):
         super().__init__(message)
+
 
 class NonexistentPillowError(Exception):
     '''Catch a pillow that does not exist'''
+
     def __init__(self, message):
         super().__init__(message)
