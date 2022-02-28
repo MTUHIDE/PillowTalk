17927b4e8098c0f341fb04fc169ce33026023036
Mark Washington <markdw88@gmail.com>
2022-02-28 17:17:04 -0500

    Implemented lock

--- pillowtalk/MotorControl.py
+++ pillowtalk/MotorControl.py
@@ -1,9 +1,7 @@
 from time import sleep
-from enum import Enum
 
 from threading import Thread, Lock
 
-import itertools
 import smbus
 
 """
@@ -18,21 +16,29 @@ DEVICE_ADDR = 0x10
 
 bus = smbus.SMBus(DEVICE_BUS)
 
+# Concurrency stuff
+# Dict to relate motor num to a thread for that motor
 motorThreads = {}
 
+# Prevents multiple requests from editing the thread dict at the same time
+runMotorLock = Lock()
+
+
 class MotorThread(Thread):
-    class MotorState(Enum):
-        RUNNING = 0
-        DONE = 1
-    
-    
+
     def __init__(self, motor: int, timeout: int):
         super().__init__()
         self.motor = motor
-        self.timeout = timeout
+
+        # Multiply timeout by 4 to sleep in quarter second increments for better responsiveness
+        self.timeout = timeout * 4
         self.running = True
 
     def run(self):
+        '''
+        TODO: Add a very descriptive comment
+        '''
+
         global bus
 
         print(f"Motor {self.motor} starting")
@@ -40,20 +46,22 @@ class MotorThread(Thread):
             if bus.read_byte_data(DEVICE_ADDR, self.motor) == 0:
                 print(f"Motor {self.motor} stopped early for some reason")
                 break
-            sleep(1)
+            sleep(0.25)
+            self.timeout -= 1
 
-        if self.running:
-            # Stop motor
-            pass
+    def stop(self):
+        '''
+        Tell this thread to stop
+        '''
 
-    def updateTimeout(self, timeout):
-        print(f"Set motor {self.motor}'s thread timeout to {timeout}")
-        self.timeout = timeout
+        self.running = False
+
 
 def stopAll():
     """
     Stop all motors
     """
+
     bus.write_byte_data(DEVICE_ADDR, 1, 0x00)
     bus.write_byte_data(DEVICE_ADDR, 2, 0x00)
     bus.write_byte_data(DEVICE_ADDR, 3, 0x00)
@@ -101,11 +109,13 @@ def deflateAll(time):
 
 
 def wait(time, motor, motor2=None):
-    '''Given a list of motors, wait a specified time and check if they got turned off by something else'''
+    '''
+    Given a list of motors, wait a specified time and check if they got turned off by something else
+    '''
+
     for x in range(time):
         if motor2 != None:
-            print(f"Motor {motor} and motor {motor2} on for {x} second(s)".format(
-                motor, motor2, x))
+            print(f"Motor {motor} and motor {motor2} on for {x} second(s)")
             if bus.read_byte_data(DEVICE_ADDR, motor) == 0 or bus.read_byte_data(DEVICE_ADDR, motor2) == 0:
                 raise MotorStoppedEarlyError("Motor Stopped Early Error")
         else:
@@ -116,7 +126,10 @@ def wait(time, motor, motor2=None):
 
 
 def checkDomain(motor, motor2=None):
-    '''Check the validity of one or two motors being called'''
+    '''
+    Check the validity of one or two motors being called
+    '''
+
     if motor < 1 or motor > 4:
         raise InternalMotorError(f"{motor} is outside domain")
     if motor2 != None:
@@ -126,32 +139,28 @@ def checkDomain(motor, motor2=None):
 
 
 def runMotor(motor: int, time: int) -> None:
-    ''' Run a motor for a specified period of time (in seconds) '''
-
     '''
-    Create a dictionary that relates motor number to a thread
-    if key exists
-        if motorThread.is_alive():
-            motorThread.running = false
-            motorThread should kill itself
-
-    new thread
+    Run a motor for a specified period of time (in seconds)
     '''
-    try:
-        checkDomain(motor)
-    except:
-        return
 
-    if motor in motorThreads and motorThreads[motor].is_alive():
-        # Update value within thread
-        motorThreads[motor].running = False
-        
+    checkDomain(motor)
+
+    runMotorLock.acquire()
+
+    if motor in motorThreads:
+        motorThreads[motor].stop()
+        motorThreads[motor].join()
+
     motorThreads[motor] = MotorThread(motor, time)
-    motorThreads[motor].start()
+
+    runMotorLock.release()
 
 
 def motorRun(time, motor):
-    '''Run the specified motor for a specified amount of time'''
+    '''
+    Run the specified motor for a specified amount of time
+    '''
+
     try:
         checkDomain(motor)
     except:
@@ -187,11 +196,11 @@ def motorRun(time, motor):
 
 
 def motorRun2(time, motor, motor2):
-    '''Run the specified motors for a specified amount of time'''
-    try:
-        checkDomain(motor, motor2)
-    except:
-        return
+    '''
+    Run the specified motors for a specified amount of time
+    '''
+
+    checkDomain(motor, motor2)
 
     # Assuming pillow 1 is left and Pillow 2 is right
     if motor == 1 or motor2 == 1:
@@ -218,195 +227,16 @@ def motorRun2(time, motor, motor2):
 
 
 def timeFormat(seconds):
-    '''Convert the provided seconds into hours, minutes, seconds'''
+    '''
+    Convert the provided seconds into hours, minutes, seconds
+    '''
+
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
     return "%d:%02d:%02d" % (hour, minutes, seconds)
-
-
-class MotorControl:
-    # Initalize pin placements and set the pins to output
-    # pin 1 inflate pillow 1, pin 2 deflate pillow1, pin 3 inflate pillow 2, pin 4 deflate pillow 2
-    def __init__(self):
-        self._DEVICE_BUS = 1
-        self._DEVICE_ADDR = 0x10
-        self._bus = smbus.SMBus(self._DEVICE_BUS)
-
-    def stopAll(self):
-        '''Stop all motors'''
-        self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0x00)
-        self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0x00)
-        self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0x00)
-        self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0x00)
-
-    # List of predefined functions that control the motors
-
-    def motor1On(self, time):
-        self.motorRun(time, 1)
-
-    def motor2On(self, time):
-        self.motorRun(time, 2)
-
-    def motor3On(self, time):
-        self.motorRun(time, 3)
-
-    def motor4On(self, time):
-        self.motorRun(time, 4)
-
-    def motor1Off(self, time):
-        self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0x00)
-
-    def motor2Off(self, time):
-        self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0x00)
-
-    def motor3Off(self, time):
-        self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0x00)
-
-    def motor4Off(self, time):
-        self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0x00)
-
-    def inflateAll(self, time):
-        self.motorRun2(time, 1, 3)
-
-    def deflateAll(self, time):
-        self.motorRun2(time, 2, 4)
-
-    def wait(self, time, motor, motor2=None):
-        '''Given a list of motors, wait a specified time and check if they got turned off by something else'''
-        for x in range(time):
-            if motor2 != None:
-                print("motor {} and motor {} on {} second".format(motor, motor2, x))
-                if self._bus.read_byte_data(self._DEVICE_ADDR, motor) == 0 or self._bus.read_byte_data(self._DEVICE_ADDR, motor2) == 0:
-                    raise MotorStoppedEarlyError("Motor Stopped Early Error")
-            else:
-                print("motor {} on {} second".format(motor, x))
-                if self._bus.read_byte_data(self._DEVICE_ADDR, motor) == 0:
-                    raise MotorStoppedEarlyError("Motor Stopped Early Error")
-            sleep(1)
-
-    def checkDomain(self, motor, motor2=None):
-        '''Check the validity of one or two motors being called'''
-        if motor < 1 or motor > 4:
-            raise InternalMotorError(f"{motor} is Outside Domain")
-        if motor2 != None:
-            if ((motor == 1 and motor2 == 2) or (motor == 3 and motor2 == 4) or (motor2 == 2 and motor == 1) or (motor2 == 4 and motor2 == 3)) and (motor2 < 1 or motor2 > 4):
-                raise InternalMotorError(
-                    f"Incompatible Motors {motor} and {motor2}")
-
-    def runMotor(self, motor: int, time: int) -> None:
-        ''' Run a motor for a specified period of time (in seconds) '''
-        try:
-            self.checkDomain(motor)
-        except:
-            return
-
-        class MotorThread(Thread):
-            def __init__(self, motorControl):
-                super().__init__()
-                self.motorControl: MotorControl = motorControl
-
-            def run(self):
-                # self.motorControl._bus
-                print("Starting")
-                sleep(5)
-                print(f"Done)")
-
-        pass
-
-    def motorRun(self, time, motor):
-        '''Run the specified motor for a specified amount of time'''
-        try:
-            self.checkDomain(motor)
-        except:
-            return
-
-        # Assuming pillow 1 is left and Pillow 2 is right
-        if motor == 1:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0xFF)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0x00)
-        if motor == 2:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0x00)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0xFF)
-        if motor == 3:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0xFF)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0x00)
-        if motor == 4:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0x00)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0xFF)
-
-        try:
-            self.wait(time, motor)
-        except:
-            return
-
-        if motor == 1 or motor == 2:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0x00)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0x00)
-        if motor == 3 or motor == 4:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0x00)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0x00)
-
-        print("motor Finished")
-
-    def motorRun2(self, time, motor, motor2):
-        '''Run the specified motors for a specified amount of time'''
-        try:
-            self.checkDomain(motor, motor2)
-        except:
-            return
-
-        # Assuming pillow 1 is left and Pillow 2 is right
-        if motor == 1 or motor2 == 1:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0xFF)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0x00)
-        if motor == 2 or motor2 == 2:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 1, 0x00)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 2, 0xFF)
-        if motor == 3 or motor2 == 3:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0xFF)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0x00)
-        if motor == 4 or motor2 == 4:
-            self._bus.write_byte_data(self._DEVICE_ADDR, 3, 0x00)
-            self._bus.write_byte_data(self._DEVICE_ADDR, 4, 0xFF)
-
-        try:
-            self.wait(time, motor, motor2)
-        except:
-            return
-
-        self.stopAll()
-
-        print("Motor Finished")
-
-    # cycle everything based on given number of loops
-    # def cycleLoop(self, inflateTime, deflateTime, waitTime, loopNumber):
-    #     totalTime = inflateTime + deflateTime + (waitTime*2-waitTime)
-    #     totalLoopTime = totalTime*loopNumber
-    #     print("Starting cycle with estimated cycle total time of {}".format(
-    #         self.timeFormat(totalLoopTime)))
-    #     for i in range(loopNumber):
-    #         print("On Loop {}".format(i))
-    #         if self.motorRun2(inflateTime, 1, 3) == -2:
-    #             break
-
-    #         sleep(waitTime)
-
-    #         if self.motorRun2(deflateTime, 2, 4) == -2:
-    #             break
-    #         if i < loopNumber-1:
-    #             sleep(waitTime)
-
-    def timeFormat(self, seconds):
-        '''Convert the provided seconds into hours, minutes, seconds'''
-        seconds = seconds % (24 * 3600)
-        hour = seconds // 3600
-        seconds %= 3600
-        minutes = seconds // 60
-        seconds %= 60
-        return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 
 class InternalMotorError(Exception):
