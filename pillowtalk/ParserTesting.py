697c8ec695a14961dab68795c58ad0873277cc1e
Ian Lawrie <iplawrie@mtu.edu>
2022-02-28 14:56:54 -0500

    added broken unittest

---
+++ pillowtalk/ParserTesting.py
@@ -0,0 +1,10 @@
+import unittest
+from TextParser import commandSearch
+
+class ParserTesting(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
