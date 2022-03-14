56528575a0bff69ba7d4a8ce7f94b60ba4091e56
Jake <jatallen@mtu.edu>
2022-03-14 16:12:34 -0400

    fixed bug with the online speech to text

--- Speech2Text.py
+++ Speech2Text.py
@@ -1,42 +1,55 @@
-
+#import threading
 import speech_recognition as sr
 from datetime import date
 from time import sleep
 from TextParser import *
 from relayControl import *
 from MotorControl import *
-motor = MotorControl()
 
-r = sr.Recognizer()
-mic = sr.Microphone()
-text = TextParser()
-wakeWord = "pillow"
+class Speech2Text:
 
-print("hello")
+    #motor = MotorControl()
+    #r = sr.Recognizer()
+    #mic = sr.Microphone()
+    #text = TextParser()
+    #wakeWord = "pillow"
 
-while True:
-    with mic as source:
-        audio = r.listen(source)
+    print("hello")
+    def listen(self):
+        motor = MotorControl()
+        r = sr.Recognizer()
+        mic = sr.Microphone()
+        text = TextParser()
+        wakeWord = "pillow"
         
-    try:
-        words = r.recognize_google(audio)
-        print(words)
+        with mic as source:
+            audio = r.listen(source, phrase_time_limit=10)
+                
+        try:
+            words = r.recognize_google(audio)
+            print(words)
 
-        command = text.commandSearch(words, wakeWord)
-        print(command)
-        if command == -2:
-            print("Command incomplete")
+            
+            command = text.commandSearch(words, wakeWord)
+            print(command)
+            if command == -2:
+                print("Command incomplete")
 
-        relay = text.returnRelay(command)
-        print(relay)
-        if relay ==-1:
-            print("Invalid Number")
-        elif relay == -2:
-            print("Invalid Action")
-        elif relay == -3:
-            print("Invalid Inflatable")
-        else:
-            motor.relayRun(relay[1], relay[0])
+            relay = text.returnRelay(command)
+            print(relay)
+            if relay ==-1:
+                print("Invalid Number")
+            elif relay == -2:
+                print("Invalid Action")
+            elif relay == -3:
+                print("Invalid Inflatable")
+            else:
+                motor.relayRun(relay[1], relay[0])
 
-    except:
-        print("Failed to understand")
+        except:
+            print("Failed to understand")
+    def __init__(self):
+        self.listen()
+
+Speech2Text()
+    
