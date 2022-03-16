59ad506bf26bd47d6dcff9e6a81a143b6e6b831d
Mark Washington <markdw88@gmail.com>
2022-03-16 18:38:56 -0400

    Test

--- pillowtalk/TextParser.py
+++ pillowtalk/TextParser.py
@@ -72,9 +72,8 @@ class TextParser:
 
     # bus 1 inflate pillow 1, bus 2 deflate pillow 1, bus 3 inflate pillow 2, bus 4 deflate pillow 2
     def returnMotor(self, commands):
-        print("returnMotor")
-        
         commands = commands[::-1]
+        print(commands)
         motor1 = None
         motor2 = None
         action = None
