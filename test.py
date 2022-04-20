3f0287f795c8231bcd514bfadfe6873682d9215d
iplawrie <55101286+iplawrie@users.noreply.github.com>
2022-04-20 17:14:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ strip = apa102.APA102(num_led=PIXELS_N)
 
 for i in range(PIXELS_N):
     strip.set_pixel(i, 255, 0, 0)
-strip.write()
+strip.show()
 time.sleep(5)
 strip.fill((0,0,0,0))
-strip.write()
+strip.show()
