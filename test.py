3e5227c05c7c1d04eba95e4251074821fbbdb33a
iplawrie <55101286+iplawrie@users.noreply.github.com>
2022-04-20 16:50:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from driver import apa102
+import apa102
 from gpiozero import LED
 import time
 
-strip = apa102.APA102(num_led=12, global_brightness=100, mosi=10, sclk=11, order='rbg')
+PIXELS_N = 12
+
+strip = apa102.APA102(num_led=PIXELS_N)
 strip.clear_strip()
 for i in range(12):
     strip.set_pixel_rgb(i, 0xFF0000)
