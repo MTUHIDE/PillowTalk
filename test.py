0de7ac8b97ca97ae3d9260a669f4d9de5ae4c60c
iplawrie <55101286+iplawrie@users.noreply.github.com>
2022-04-20 17:14:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import machine, apa102
+import apa102
 from gpiozero import LED
 import time
 
 PIXELS_N = 12
 
-strip = apa102.APA102(machine.Pin(5), machine.Pin(4), num_led=PIXELS_N)
+strip = apa102.APA102(num_led=PIXELS_N)
 
 for i in range(PIXELS_N):
     strip.set_pixel(i, 255, 0, 0)
