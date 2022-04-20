3c5c0ef715f65ae52c93ecfbbdc2827ff5f13db0
iplawrie <55101286+iplawrie@users.noreply.github.com>
2022-04-20 17:04:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import time
 PIXELS_N = 12
 
 strip = apa102.APA102(num_led=PIXELS_N)
-strip.clear_strip()
+power = LED(5)
+power.on()
 for i in range(12):
-    strip.set_pixel_rgb(i, 0xFF0000)
+    strip.set_pixel(i, 255, 0, 0)
 strip.show()
 time.sleep(5)
-strip.clear_strip()
+power.off()
